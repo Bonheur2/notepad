ae63bf309868294f8944ffa563bdafa4eb4a9edf
Bonheur2 <blissbonheur1@gmail.com>
2026-07-23 05:18:47 -1000

    Update application database

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -4,15 +4,17 @@ import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'tables/notes_table.dart';
+import 'tables/tasks_table.dart';
+import 'tables/note_revisions_table.dart';
 
 part 'app_database.g.dart';
 
-@DriftDatabase(tables: [Notes])
+@DriftDatabase(tables: [Notes, Tasks, NoteRevisions])
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(_openConnection());
 
   @override
-  int get schemaVersion => 2;
+  int get schemaVersion => 5;
 
   @override
   MigrationStrategy get migration => MigrationStrategy(
@@ -22,6 +24,15 @@ class AppDatabase extends _$AppDatabase {
         onUpgrade: (m, from, to) async {
           if (from < 2) {
             await m.addColumn(notes, notes.category);
+          }
+          if (from < 3) {
+            await m.createTable(tasks);
+          }
+          if (from < 4) {
+            await m.createTable(noteRevisions);
+          }
+          if (from < 5) {
+            await m.addColumn(notes, notes.sketchPath);
           }
         },
       );
