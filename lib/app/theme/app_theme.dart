43d5984048b03dfea7cf155f55be0dadf1bd2e34
Bonheur2 <blissbonheur1@gmail.com>
2026-07-23 05:18:46 -1000

    Update application theme

--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -22,4 +22,28 @@ class AppTheme {
       ),
     );
   }
+
+  static ThemeData get highContrastLight {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.indigo,
+        brightness: Brightness.light,
+        contrastLevel: 1.0,
+      ),
+    );
+  }
+
+  static ThemeData get highContrastDark {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.indigo,
+        brightness: Brightness.dark,
+        contrastLevel: 1.0,
+      ),
+    );
+  }
 }
